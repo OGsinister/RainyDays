45616171c8ea4b41994f40263f8aef613a644514
Sinister <serpichenkolou@gmail.ru>
2023-08-12 23:04:28 +0500

    Забыл про сущность favorite в database, добавил

--- core_db/src/main/java/com/example/core_db/AppDatabase.kt
+++ core_db/src/main/java/com/example/core_db/AppDatabase.kt
@@ -3,11 +3,13 @@ package com.example.core_db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.core_db.dao.LocationDao
+import com.example.core_db.models.FavoriteLocationDb
 import com.example.core_db.models.LocationDb
 
 @Database(
-    entities = [LocationDb::class],
-    version = 1
+    entities = [LocationDb::class,FavoriteLocationDb::class],
+    version = 1,
+    exportSchema = false
 )
 abstract class AppDatabase(): RoomDatabase() {
     abstract val locationDao: LocationDao
