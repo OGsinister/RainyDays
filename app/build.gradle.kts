4b94c6b704ea7404676a2ea5bc56eda1f9af668f
Sinister <serpichenkolou@gmail.ru>
2023-08-12 23:03:40 +0500

    оказалось, что navigation работает с api 34 и выше, пришлось повысить apiшку(((((((

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.example.rainydays"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.rainydays"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
