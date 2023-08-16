f1b9b29c4dbc761b142fed359c2d6c678a910807
Sinister <serpichenkolou@gmail.ru>
2023-08-17 01:25:34 +0500

    Добавил новые endpoint

--- core_network/src/main/java/com/example/core_network/utils/ApiEndPoints.kt
+++ core_network/src/main/java/com/example/core_network/utils/ApiEndPoints.kt
@@ -1,9 +1,11 @@
 package com.example.core_network.utils
 
-object Constants {
+object ApiEndPoints {
     const val BASE_URL = "https://api.weatherapi.com/v1/"
     const val FORECAST = "forecast.json?"
+    const val DAYS = "&days=1"
     const val CURRENT = "current.json?"
     const val API_KEY = "key=abae908adcac4bd69b5193745230108"
     const val AQI = "&aqi=no"
+    const val ALERTS = "&alerts=no"
 }
